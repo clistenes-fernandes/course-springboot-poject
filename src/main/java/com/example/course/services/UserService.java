0b6f4b5ac85d9f7dd7990592038ab4c088e2aa05
clistenes-fernandes <clisteness.fernandes@gmail.com>
2021-02-05 17:17:12 +0000

    User Update

--- src/main/java/com/example/course/services/UserService.java
+++ src/main/java/com/example/course/services/UserService.java
@@ -31,5 +31,18 @@ public class UserService {
 	public void delete(Long id) {
 		userRepository.deleteById(id);
 	}
+	
+	public User update(Long id, User user) {
+		//Monitored user entity
+		User entity = userRepository.getOne(id);
+		updateData(entity, user);
+		return userRepository.save(entity);
+	}
+
+	private void updateData(User entity, User user) {
+		entity.setName(user.getName());
+		entity.setEmail(user.getEmail());
+		entity.setPhone(user.getPhone());	
+	}
 
 }
